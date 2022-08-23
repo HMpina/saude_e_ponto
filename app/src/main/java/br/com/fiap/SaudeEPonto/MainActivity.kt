9d773fd2998c7025429d2a195afc5b54290c1b7f
HMpina <henrique.marques.pina@gmail.com>
2022-08-22 23:13:15 -0300

    Colors and date persistence applied

--- app/src/main/java/br/com/fiap/SaudeEPonto/MainActivity.kt
+++ app/src/main/java/br/com/fiap/SaudeEPonto/MainActivity.kt
@@ -3,13 +3,14 @@ package br.com.fiap.SaudeEPonto
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 lateinit var buttonLogin: Button
 lateinit var textEmail : EditText
 lateinit var textPassword : EditText
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,21 +21,21 @@ class MainActivity : AppCompatActivity() {
         textEmail = findViewById(R.id.editTextEmailAddress)
         textPassword = findViewById(R.id.editTextPassword)
 
-
         // Ouvinte de click do botão de login
         buttonLogin.setOnClickListener {
             login()
         }
-
-
-
     }
 
     private fun login() {
 
-
-        val openHome = Intent(this, HomeActivity::class.java)
-        startActivity(openHome)
+        val msg: String = textEmail.text.toString()
+        if (msg.trim().length<5) {
+            Toast.makeText(applicationContext, "Please enter a valid email", Toast.LENGTH_SHORT).show()
+        }else{
+            val openHome = Intent(this, HomeActivity::class.java)
+            startActivity(openHome)
+        }
 
     }
 }
